fe9297332f2e8e955cc656cd0d3b26c9c0403b8e
Gwan Thanakrit <thanakrit.j@thoughtworks.com>
2020-06-08 13:57:38 +0700

    add more endpoints

--- index.js
+++ index.js
@@ -2,10 +2,28 @@ const express = require('express')
 const expressGraphQL = require('express-graphql')
 const app = express()
 
+const {books, authors} = require('./library')
 const bookSchema = require('./schema')
 
-app.use('/graphql', expressGraphQL({
+// GraphQL
+app.get('/graphql', expressGraphQL({
+    schema: bookSchema,
+    graphiql: false
+}))
+
+app.post('/graphql', expressGraphQL({
     schema: bookSchema,
     graphiql: true
 }))
+
+// REST
+app.get('/books/:id', (req, res) => {
+    res.json(books.find(book => book.id == req.params.id))
+})
+
+app.get('/authors/:id', (req, res) => {
+    res.json(authors.find(author => author.id == req.params.id))
+})
+
+
 app.listen(5000, () => console.log("Server is running"))
