e227be3d53a5db04326fcf3dcfa61e8d6b831233
Gwan Thanakrit <thanakrit.j@thoughtworks.com>
2020-06-08 15:38:42 +0700

    Add post method for rest api

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 const express = require('express')
 const expressGraphQL = require('express-graphql')
 const app = express()
+var bodyParser = require('body-parser')
 
 const {books, authors} = require('./library')
 const bookSchema = require('./schema')
 
+app.use(bodyParser.json())
+app.listen(5000, () => console.log("Server is running"))
+
 // GraphQL
 app.get('/graphql', expressGraphQL({
     schema: bookSchema,
-    graphiql: false
+    graphiql: true
 }))
 
 app.post('/graphql', expressGraphQL({
     schema: bookSchema,
-    graphiql: true
+    graphiql: false
 }))
 
 // REST
@@ -25,5 +29,16 @@ app.get('/authors/:id', (req, res) => {
     res.json(authors.find(author => author.id == req.params.id))
 })
 
+app.get('/books', (_, res) => {
+    res.json(books)
+})
 
-app.listen(5000, () => console.log("Server is running"))
+app.post('/books', (req, res) => {
+    console.log(req.body)
+    books.push(req.body)
+    res.status(201).json(req.body)
+})
+
+app.get('/authors', (_, res) => {
+    res.json(authors)
+})
