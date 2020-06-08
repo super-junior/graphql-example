40fb4281c5d3015ab2dd94175bd3ce0aed7f8c95
Gwan Thanakrit <thanakrit.j@thoughtworks.com>
2020-06-08 15:46:17 +0700

    Add delete method

--- index.js
+++ index.js
@@ -25,10 +25,6 @@ app.get('/books/:id', (req, res) => {
     res.json(books.find(book => book.id == req.params.id))
 })
 
-app.get('/authors/:id', (req, res) => {
-    res.json(authors.find(author => author.id == req.params.id))
-})
-
 app.get('/books', (_, res) => {
     res.json(books)
 })
@@ -37,6 +33,20 @@ app.post('/books', (req, res) => {
     console.log(req.body)
     books.push(req.body)
     res.status(201).json(req.body)
+})
+
+app.delete('/books/:id', (req, res) => {
+    const deletedIndex = books.findIndex(book => book.id == req.params.id)
+    if (deletedIndex != -1) {
+        books.splice(deletedIndex, 1)
+        res.status(204).send()
+    } else {
+        res.status(404).send()
+    }
+})
+
+app.get('/authors/:id', (req, res) => {
+    res.json(authors.find(author => author.id == req.params.id))
 })
 
 app.get('/authors', (_, res) => {
